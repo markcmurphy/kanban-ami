fd77d6622ee60a5ef0bfab57a296c844ed23cca7
Mark Murphy <mark@murphymark.me>
2018-07-09 15:16:37 -0500

    connected to MongoDB

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,22 @@
 const app = angular.module('MyApp', []);
 
-app.controller('MyController', function(){
-this.foo = 'bar';
+app.controller('MyController', ['$http', function($http){
+
+this.createTodo = function(){
+$http({
+method:'POST',
+url: '/todos',
+data: {
+description: this.description,
+complete: this.complete
+}
+}).then(function(response){
+console.log(response);
+}, function(){
+console.log('error');
 });
+}
+
+
+// end of MyController
+}]);
--- public/js/server.js
+++ public/js/server.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+
+app.listen(3000, () => {
+  console.log('listening');
+});
+
 app.use(express.static('public'));
+
+
+mongoose.connect('mongodb://localhost:27017/kanban-ami');
+
+mongoose.connection.once('open', () => {
+  console.log('connected to mongo');
+});
