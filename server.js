1f0ff6089598c7df2299c0d0a15aa4308ef2062b
Mark Murphy <mark@murphymark.me>
2018-09-24 15:28:12 -0500

    Added listsController to server.js

--- server.js
+++ server.js
@@ -5,16 +5,18 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-
-app.listen(3000, () => {
-  console.log('listening');
-});
-
 app.use(express.static('public'));
 
 const friendsController = require('./controllers/friends.js');
 app.use('/friends', friendsController);
 
+const listsController = require('./controllers/lists.js');
+app.use('/lists', listsController);
+
+
+app.listen(3000, () => {
+  console.log('listening');
+});
 
 mongoose.connect('mongodb://localhost:27017/kanban-ami');
 
